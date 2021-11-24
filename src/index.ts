70c5ba28ff3e57b5c161c6f82c510b67a543d30c
Takeshi <iwatakeshi@gmail.com>
2021-11-23 18:28:08 -0700

    Export types from each component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { default as Link } from './components/Link'
-export { default as Button } from './components/Button'
-export { default as IconButton} from './components/IconButton'
+export { default as Link } from "./components/Link";
+export { default as Button } from "./components/Button";
+export { default as IconButton } from "./components/IconButton";
+export * from "./components/Link";
+export * from "./components/Button";
+export * from "./components/IconButton";
